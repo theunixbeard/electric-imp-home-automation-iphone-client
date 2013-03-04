c18fe17bd025af44c6bfd1ce76d1ebfa57402d28
Ben Gelsey <ben.gelsey@gmail.com>
2013-03-03 18:47:24 -0800

    Added navigation bar to control screen, next step is to build Outlet, User, and Schedule models.

---
+++ Home-Automation1/OutletPowerController.h
@@ -0,0 +1,13 @@
+//
+//  OutletPowerController.h
+//  Home-Automation1
+//
+//  Created by Ben Gelsey on 3/3/13.
+//  Copyright (c) 2013 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OutletPowerController : UITableViewController
+
+@end
---
+++ Home-Automation1/OutletPowerController.m
@@ -0,0 +1,132 @@
+//
+//  OutletPowerController.m
+//  Home-Automation1
+//
+//  Created by Ben Gelsey on 3/3/13.
+//  Copyright (c) 2013 Self. All rights reserved.
+//
+
+#import "OutletPowerController.h"
+#import "AFNetworking.h"
+
+@interface OutletPowerController ()
+
+@end
+
+@implementation OutletPowerController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad{
+  [super viewDidLoad];
+
+  // Uncomment the following line to preserve selection between presentations.
+  // self.clearsSelectionOnViewWillAppear = NO;
+ 
+  // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+   self.navigationItem.rightBarButtonItem = self.editButtonItem;
+  NSURL *url = [NSURL URLWithString:@"http://localhost:9292/test.json"];
+  NSURLRequest *request = [NSURLRequest requestWithURL:url];
+  AFJSONRequestOperation *operation;
+  operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request
+                                                              success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                                                                NSLog(@"Response: %@", JSON);
+                                                              } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+                                                                NSLog(@"Received an HTTP %d", response.statusCode);
+                                                                NSLog(@"The error was: %@", error);
+                                                              }];
+  [operation start];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
